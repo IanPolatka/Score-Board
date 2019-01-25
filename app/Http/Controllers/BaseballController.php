cfc365da73e49de7652866b082361764943909ed
Ian Polatka <ianpolatka@gmail.com>
2019-01-25 16:07:49 -0500

    Baseball added.

---
+++ app/Http/Controllers/BaseballController.php
@@ -0,0 +1,475 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+
+use App\Baseball;
+use App\BaseballScores;
+use App\Team;
+use App\TeamMeta;
+use App\Time;
+use App\Tournament;
+use App\Year;
+
+use Session;
+
+use Auth;
+
+use Illuminate\Http\Request;
+
+class BaseballController extends Controller
+{
+    
+    public function __construct() 
+    {
+      $this->middleware('auth', ['only' => [ 'create', 'edit', 'editScore', 'delete' ]]);
+    }
+
+
+
+    public function index()
+    {
+
+        $teams = Team::orderBy('school_name')->get();
+
+        $todaysGames = Baseball::where('date', Carbon::today('America/New_York'))->get();
+
+        $yesterdaysGames = Baseball::where('date', Carbon::yesterday('America/New_York'))->get();
+
+        $tomorrowsGames = Baseball::where('date', Carbon::tomorrow('America/New_York'))->get();
+
+        $games = Baseball::all();
+
+        return view('sports.baseball.index', compact('games', 'teams', 'todaysGames', 'tomorrowsGames', 'yesterdaysGames'));
+    }
+
+
+
+    public function create()
+    {
+        $teams = Team::orderBy('school_name')->get();
+
+        $times = Time::all();
+
+        $years = Year::all();
+
+        return view('sports.baseball.create', compact('teams','times','years'));
+    }
+
+
+
+    public function store(Request $request)
+    {
+        $user_id = Auth::user()->id;
+
+        $this->validate(request(), [
+            'year_id'           => 'required',
+            'team_level'        => 'required',
+            'date'              => 'date|required',
+            'away_team_id'      => 'required',
+            'home_team_id'      => 'required',
+            'time_id'           => 'required'
+        ],
+        [
+            'year_id.required'          =>  'Please select a school year.',
+            'team_level.required'       =>  'Please select a team level.',
+            'away_team_id.required'     =>  'Please select an away team.',
+            'home_team_id.required'     =>  'Please select a home team.',
+            'time_id.required'          =>  'Please select a game time.'
+        ]);
+
+        Baseball::create([
+            'year_id'       => request('year_id'),
+            'team_level'    => request('team_level'),
+            'date'          => request('date'),
+            'scrimmage'     => request('scrimmage'),
+            'tournament_name' => request('tournament_name'),
+            'location'      => request('location'),
+            'away_team_id'  => request('away_team_id'),
+            'home_team_id'  => request('home_team_id'),
+            'time_id'       => request('time_id'),
+            'district_game' => request('district_game'),
+            'created_by'    => $user_id
+        ]);
+
+        Session::flash('success', 'Baseball Game Has Been Created');
+
+        return redirect('/baseball');
+    }
+
+
+
+    public function show($id)
+    {
+        $game = Baseball::where('id', $id)->with('away_team')
+                                     ->with('home_team')
+                                     ->with('home_team_district')
+                                     ->with('away_team')
+                                     ->with('away_team_district')
+                                     ->with('game_time')
+                                     ->with('user_created')
+                                     ->with('user_modified')
+                                     ->with('scores')
+                                     ->first();
+
+        return view('sports.baseball.show', compact('game'));
+    }
+
+
+
+    public function edit($id)
+    {
+
+        $teams = Team::orderBy('school_name')->get();
+
+        $times = Time::all();
+
+        $years = Year::all();
+
+        $match = Baseball::where('id',$id)->with('away_team')
+                                     ->with('home_team')
+                                     ->with('away_team')
+                                     ->with('game_time')
+                                     ->with('user_created')
+                                     ->with('user_modified')
+                                     ->with('scores')
+                                     ->first();
+
+        return view('sports.baseball.edit', compact('match', 'teams','times','years'));
+    }
+
+
+
+    public function update(Request $request, $id)
+    {
+        $user_id = Auth::user()->id;
+
+        $this->validate(request(), [
+            'year_id'           => 'required',
+            'team_level'        => 'required',
+            'date'              => 'date|required',
+            'away_team_id'      => 'required',
+            'home_team_id'      => 'required',
+            'time_id'           => 'required'
+        ],
+        [
+            'year_id.required'          =>  'Please select a school year.',
+            'team_level.required'       =>  'Please select a team level.',
+            'away_team_id.required'     =>  'Please select an away team.',
+            'home_team_id.required'     =>  'Please select a home team.',
+            'time_id.required'          =>  'Please select a game time.'
+        ]);
+
+        $game = Baseball::findOrFail($id);
+        $game->year_id = request('year_id');
+        $game->team_level = request('team_level');
+        $game->date = request('date');
+        $game->scrimmage = request('scrimmage');
+        $game->tournament_name = request('tournament_name');
+        $game->away_team_id = request('away_team_id');
+        $game->home_team_id = request('home_team_id');
+        $game->time_id = request('time_id');
+        $game->district_game = request('district_game');
+        $game->location     = request('location');
+        $game->modified_by   = $user_id;
+
+        $game->update();
+
+        Session::flash('success', 'Baseball Game Has Been Updated');
+
+        return redirect('/baseball/'.$id);
+    }
+
+    public function editScore($id)
+    {
+
+        $scores = BaseballScores::where('game_id', $id)->get();
+
+        $match = Baseball::where('id', $id)->with('away_team')
+                                     ->with('home_team')
+                                     ->with('away_team')
+                                     ->with('game_time')
+                                     ->with('user_created')
+                                     ->with('user_modified')
+                                     ->with('scores')
+                                     ->first();
+
+        return view('sports.baseball.edit-score', compact('away_team_ties', 'away_losses', 'away_wins', 'home_losses', 'home_team_ties', 'home_wins', 'game', 'match', 'scores', 'teams','times','years'));
+    }
+
+
+
+    public function gameUpdate(Request $request, $id)
+    {
+        $user_id = Auth::user()->id;
+
+        $match = Baseball::where('id', $id)->get();
+
+        $game = Baseball::findOrFail($id);
+        $game->inning = request('inning');
+        $game->away_team_final_score = request('away_team_final_score');
+        $game->home_team_final_score = request('home_team_final_score');
+        $game->winning_team = request('winning_team');
+        $game->losing_team = request('losing_team');
+        $game->modified_by   = $user_id;
+
+        $game->update();
+
+        Session::flash('success', 'Baseball Game Has Been Updated');
+
+        return back();
+    }
+
+
+
+    public function destroy($id)
+    {
+        $game = Baseball::find($id);
+        $game->delete();
+        return redirect('/baseball');
+    }
+
+
+
+    public function teamSchedule($team)
+    {
+
+        $id = Team::where('school_name', $team)->pluck('id');
+
+        $selectedTeam = Team::where('school_name', $team)->first();
+
+        $year = Year::pluck('id')->first();
+
+        $wins = Baseball::where('winning_team', $id)->where('team_level', 1)->count();
+
+        $losses = Baseball::where('losing_team', $id)->where('team_level', 1)->count();
+
+        $teams = Team::orderBy('school_name')->get();
+
+        $varsity = Baseball::with('away_team')
+                               ->with('home_team')
+                               ->with('away_team_district')
+                               ->with('home_team_district')
+                               ->where(function ($query) use ($id) {
+                                    $query->where('away_team_id', '=' , $id)
+                                    ->orWhere('home_team_id', '=', $id);
+                               })
+                               ->where('team_level', 1)
+                               // ->where('team_level', 1)
+                               ->orderBy('date')
+                               ->get();
+
+        $juniorvarsity = Baseball::with('away_team')
+                               ->with('home_team')
+                               ->where(function ($query) use ($id) {
+                                    $query->where('away_team_id', '=' , $id)
+                                    ->orWhere('home_team_id', '=', $id);
+                                })
+                               ->where('team_level', 2)
+                               ->orderBy('date')
+                               ->get();
+
+        $freshman = Baseball::with('away_team')
+                               ->with('home_team')
+                               ->where(function ($query) use ($id) {
+                                    $query->where('away_team_id', '=' , $id)
+                                    ->orWhere('home_team_id', '=', $id);
+                                })
+                               ->where('team_level', 3)
+                               ->orderBy('date')
+                               ->get();
+
+        return view('sports.baseball.teamschedule', compact('id', 'selectedTeam', 'team', 'teams', 'varsity', 'juniorvarsity', 'freshman', 'wins', 'losses'));
+
+    }
+
+
+    public function scoreCreate($id)
+    {
+
+        $user_id = Auth::user()->id;
+
+        BaseballScores::create([
+            'game_id'          =>  $id,
+            'away_team_score'   =>  0,
+            'home_team_score'   =>  0,
+            'created_by'        =>  $user_id
+        ]);
+
+        Session::flash('success', 'Quarter Has Been Added');
+
+        return back();
+
+    }
+
+
+
+    public function storeGameHalf(Request $request, $id)
+    {
+
+        $user_id = Auth::user()->id;
+
+        $this->validate(request(), [
+            'away_team_score'       =>  'required|numeric',
+            'home_team_score'       =>  'required|numeric',
+        ]);
+
+        $half = BaseballScores::findOrFail($id);
+        $half->away_team_score  = request('away_team_score');
+        $half->home_team_score  = request('home_team_score');
+        $half->modified_by = $user_id;
+
+        $half->update();
+
+        Session::flash('success', 'Score Has Been Updated');
+
+        return back();
+
+    }
+
+
+
+    public function scoreDelete($id)
+    {
+        BaseballScores::find($id)->delete();
+
+        Session::flash('success', 'Quarter Has Been Deleted');
+
+        return back();
+    }
+
+
+
+    public function apiGameId($id)
+    {
+
+        $game = Baseball::where('id', $id)->with('away_team')
+                                     ->with('home_team')
+                                     ->with('away_team')
+                                     ->with('game_time')
+                                     ->with('user_created')
+                                     ->with('user_modified')
+                                     ->with('scores')
+                                     ->first();
+
+        return $game;
+
+    }
+
+
+
+    public function apiTeamSchedule($year, $team, $teamlevel)
+    {
+
+        $theteam = Team::where('school_name', '=', $team)->pluck('id');
+        $theYear = Year::where('year', $year)->pluck('id')->first();
+
+        $game = Baseball::with('away_team')
+                                     ->with('home_team')
+                                     ->with('away_team')
+                                     ->with('game_time')
+                                     ->with('user_created')
+                                     ->with('user_modified')
+                                     ->with('scores')
+                                     ->with('the_year')
+                                     ->where('year_id', $theYear)
+                                     ->where(function ($query) use ($theteam) {
+                                        $query->where('away_team_id', '=' , $theteam)
+                                        ->orWhere('home_team_id', '=', $theteam);
+                                     })
+                                     ->where('team_level', $teamlevel)
+                                     
+                                     ->orderBy('date','asc')
+                                     ->get();
+
+        return $game;
+
+    }
+
+
+
+    public function todaysEvents($team)
+    {
+
+        $theteam = Team::where('school_name', '=', $team)->pluck('id');
+
+        $game = Baseball::with('away_team')
+                                     ->with('home_team')
+                                     ->with('away_team')
+                                     ->with('game_time')
+                                     ->with('user_created')
+                                     ->with('user_modified')
+                                     ->with('scores')
+                                     ->with('the_year')
+                                     ->where(function ($query) use ($theteam) {
+                                        $query->where('away_team_id', '=' , $theteam)
+                                        ->orWhere('home_team_id', '=', $theteam);
+                                     })
+                                     ->where('team_level', 1)
+                                     ->where('date', Carbon::today('America/New_York'))
+                                     ->orderBy('date','asc')
+                                     ->get();
+
+        return $game;
+
+    }
+
+
+
+    public function singleMatch($id)
+    {
+
+        $game = Baseball::with('away_team')
+                                     ->with('home_team')
+                                     ->with('away_team')
+                                     ->with('game_time')
+                                     ->with('user_created')
+                                     ->with('user_modified')
+                                     ->with('scores')
+                                     ->with('the_year')
+                                     ->first();
+
+        return $game;
+
+    }
+
+
+
+    public function yearSummary($year, $team)
+    {
+
+        // return $year;
+        $selectedyear = Year::where('year', $year)->pluck('year');
+        $selectedyearid = Year::where('year', $year)->pluck('id');
+
+        $selectedteam = Team::where('school_name', $team)->get();
+
+        $selectedteamid =   Team::where('school_name', $team)->pluck('id');
+
+        $the_standings = \DB::select('SELECT
+                            school_name AS Team, logo, Sum(W) AS Wins, Sum(L) AS Losses, SUM(F) as F, SUM(A) AS A, 
+                        FROM(
+
+                            SELECT
+                                home_team_id Team,
+                                IF(home_team_final_score > away_team_final_score,1,0) W,
+                                IF(home_team_final_score < away_team_final_score,1,0) L,
+                                home_team_final_score F,
+                                away_team_final_score A,
+                                
+                            FROM baseball
+                            WHERE year_id = ? AND team_level = 1
+                              
+                        )
+                        as tot
+                        JOIN teams t ON tot.Team=t.id
+                        JOIN teams_meta t ON tot.Team=t.id
+                        WHERE school_name = ?
+                        GROUP BY Team
+                        ORDER BY wins DESC, losses ASC, school_name', array($selectedyearid[0], $selectedyearid[0], $selectedteam[0]['school_name']));
+
+        return $the_standings;
+
+    }
+}
---
+++ resources/views/sports/baseball/create.blade.php
@@ -0,0 +1,237 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="breadcrumb-section">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col">
+
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item active">Baseball</li>
+                  </ol>
+                </nav>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+	
+			<h3 class="title">Create Game</h3>
+
+            <div class="card">
+
+                <div class="card-body">
+
+                    <form action="{{route('baseball-create-game')}}" method="POST">
+                        {{csrf_field()}}
+
+                        <div class="form-group">
+                            <label for="year_id">School Year</label>
+                            <select class="form-control" id="year_id" name="year_id" @if ($errors->has('year_id')) style="border-color: #dc3545;" @endif>
+                                <option value="">Select A Year</option>
+                                @foreach($years as $year)
+                                    <option value="{{$year->id}}" @if (old('year_id') == $year->id) selected @endif>{{$year->year}}</option>
+                                @endforeach
+                            </select>
+
+                            @if ($errors->has('year_id'))
+                                <span style="width: 100%;margin-top: .25rem;font-size: 80%;color: #dc3545;">
+                                    <strong>{{ $errors->first('year_id') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="team_level">Team Level</label>
+                            <select class="form-control" id="team_level" name="team_level" @if ($errors->has('team_level')) style="border-color: #dc3545;" @endif>
+                                <option value="">Select A Team Level</option>
+                                <option value="1" @if (old('team_level') == '1') selected @endif>Varsity</option>
+                                <option value="2" @if (old('team_level') == '2') selected @endif>Junior Varsity</option>
+                                <option value="3" @if (old('team_level') == '3') selected @endif>Freshman</option>
+                            </select>
+
+                            @if ($errors->has('team_level'))
+                                <span style="width: 100%;margin-top: .25rem;font-size: 80%;color: #dc3545;">
+                                    <strong>{{ $errors->first('team_level') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="date">Date</label>
+                            <input type="text" class="form-control{{ $errors->has('date') ? ' is-invalid' : '' }}" name="date" id="datepicker" aria-describedby="Date" placeholder="Date" value="{{ old('date') }}">
+
+                            @if ($errors->has('date'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('date') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="scrimmage">Is this game a scrimmage?</label>
+                            <select class="form-control" id="" name="scrimmage">
+                                <option value="0" @if (old('scrimmage') == '0') selected @endif>No</option>
+                                <option value="1" @if (old('scrimmage') == '1') selected @endif>Yes</option>
+                            </select>
+
+                            @if ($errors->has('scrimmage'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('scrimmage') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="tournament_name">Is this game part of a tournament?  If so, what is the tournament name?</label>
+                            <input type="text" class="form-control" id="tournament_name" name="tournament_name" value="{{ old('tournament_name') }}">
+
+                            @if ($errors->has('tournament_name'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('tournament_name') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+
+                        <hr>
+
+                        <label>Is this game at a neutral location</label>
+
+                        <div class="form-group form-inline">
+
+                            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                              <label class="btn btn-small btn-outline-primary active">
+                                <input type="radio" name="location-place" id="no" value="no" autocomplete="off" checked> No
+                              </label>
+                              <label class="btn btn-small btn-outline-primary">
+                                <input type="radio" name="location-place" id="yes" value="yes" autocomplete="off"> Yes
+                              </label>
+                            </div>
+
+                        </div>
+
+                        <div class="form-group location">
+
+                            <label for="away_team_id">Game Location</label>
+                            <input type="text" class="form-control" name="location" value="{{ old('location') }}">
+
+                            @if ($errors->has('location'))
+                                <span style="width: 100%;margin-top: .25rem;font-size: 80%;color: #dc3545;">
+                                    <strong>{{ $errors->first('location') }}</strong>
+                                </span>
+                            @endif
+
+                        </div>
+
+                        <hr>
+
+                        <div class="form-group">
+                            <label for="away_team_id">Away Team</label>
+                            <select class="form-control" id="" name="away_team_id" @if ($errors->has('away_team_id')) style="border-color: #dc3545;" @endif>
+                                <option value="">Select A Team</option>
+                                @foreach($teams as $team)
+                                    <option value="{{$team->id}}" @if (old('away_team_id') == $team->id) selected @endif>{{$team->school_name}} ({{$team->state}})</option>
+                                @endforeach
+                            </select>
+
+                            @if ($errors->has('away_team_id'))
+                                <span style="width: 100%;margin-top: .25rem;font-size: 80%;color: #dc3545;">
+                                    <strong>{{ $errors->first('away_team_id') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="home_team_id">Home Team</label>
+                            <select class="form-control" id="" name="home_team_id" @if ($errors->has('home_team_id')) style="border-color: #dc3545;" @endif>
+                                <option value="">Select A Team</option>
+                                @foreach($teams as $team)
+                                    <option value="{{$team->id}}" @if (old('home_team_id') == $team->id) selected @endif>{{$team->school_name}} ({{$team->state}})</option>
+                                @endforeach
+                            </select>
+
+                            @if ($errors->has('home_team_id'))
+                                <span style="width: 100%;margin-top: .25rem;font-size: 80%;color: #dc3545;">
+                                    <strong>{{ $errors->first('home_team_id') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="time_id">Game Time</label>
+                            <select class="form-control" id="" name="time_id">
+                                <option value="" @if (old('time_id') === '') selected @endif>Please Select A Time</option>
+                                @foreach($times as $time)
+                                    <option value="{{$time->id}}" @if (old('time_id') == $time->id) selected @endif>{{$time->time}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="district_game">District Game</label>
+                            <select class="form-control" id="" name="district_game">
+                                <option value="0" @if (old('district_game') == '0') selected @endif>No</option>
+                                <option value="1" @if (old('district_game') == '1') selected @endif>Yes</option>
+                            </select>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Create</button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+
+		</div>
+
+	</div>
+
+</div>
+@endsection
+
+@section('javascript')
+<script>
+
+    $('.location').hide();
+
+    // $( "#neutral" ).on( "click", function() {
+    //     var selectedValue = $(this).val();
+    //     if(selectedValue == 'No') {
+    //         console.log(selectedValue);
+    //     } else {
+    //         console.log(selectedValue);
+    //     }
+    // });
+
+$(document).ready(function(){
+
+    $('input:radio[name=location-place]').change(function() {
+        if (this.value == 'no') {
+            $('.location').slideToggle();
+        }
+        else if (this.value == 'yes') {
+            $('.location').slideToggle();
+        }
+    });
+
+});
+
+</script>
+@stop
