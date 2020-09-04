24aafccdbb84782b9d8a81b312b8a5c2a12d921d
Ian Polatka <ianpolatka@gmail.com>
2020-09-04 13:48:49 -0400

    Remove the register link in the header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -134,9 +134,6 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
                         @else
                             @role(['superadministrator'])
                             <li class="nav-item dropdown">
